323a4283b846a9e62c9eb7b397e8d6d5fd3c43f3
Juan Antonio Fernández Ruiz <juanafr@us.es>
2023-02-24 11:41:05 +0100

    spatie-laravel-permissions - config

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+
+use Spatie\Permission\Traits\HasRoles;
+
+
 
 class User extends Authenticatable
 {
