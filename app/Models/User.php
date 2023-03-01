8bd05973a3f1554fa4aa5bf243e3a735b23bdbd9
Juan Antonio Fernández Ruiz <juanafr@us.es>
2023-03-01 09:02:45 +0100

    user Models -  use trait HasRole

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
