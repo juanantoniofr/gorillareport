4c752034efe0d6bd3d1ee97d0cf797c7c136b5c0
Juan Antonio Fernández Ruiz <juanafr@us.es>
2023-03-02 11:06:05 +0100

    API-Controller

--- app/Http/Controllers/ApiAuthController.php
+++ app/Http/Controllers/ApiAuthController.php
@@ -8,5 +8,26 @@ use Illuminate\Http\Request;
 
 class ApiAuthController extends Controller
 {
+    
     //
+    public function login(Request $request){
+        //valida las credenciales del usuario
+        if (!Auth::attempt($request->only('email', 'password'))){
+            return response()->json([
+                'message' => 'Invalid access credentials'
+            ], 401);
+        }
+
+        //Busca al usuario en la base de datos
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        //Genera un nuevo token para el usuario
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        //devuelve una respuesta JSON con el token generado y el tipo de token
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ]);
+    }
 }
