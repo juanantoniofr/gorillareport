85923fd5e927e7aa92d60cb894f0b9a7e2bafdb6
Juan Antonio Fernández Ruiz <juanafr@us.es>
2023-03-02 13:06:20 +0100

    login api route

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('/api/login', [ApiAuthController::class, 'login']); 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
-Route::post('/login', [AuthController::class, 'login']);
+
